8ac1d6c6fedb58508ab480617be660a12973e37b
Fadi Hania <fhania@gmail.com>
2023-04-30 21:16:07 +0300

    Prepare sample post type and data

---
+++ MinimalApiResponses/Data.cs
@@ -0,0 +1,14 @@
+﻿using MinimalApiResponses.Models;
+
+namespace MinimalApiResponses;
+
+public class Data
+{
+    public static List<Post> Posts = new()
+    {
+        new Post { Id = 1, Title = "Post 1", Content = "Post 1 content", Author = "Ahmad Ali" },
+        new Post { Id = 2, Title = "Post 2", Content = "Post 2 content", Author = "Saeed Mahmoud" },
+        new Post { Id = 3, Title = "Post 3", Content = "Post 3 content", Author = "Osama Masoud" },
+        new Post { Id = 4, Title = "Post 4", Content = "Post 4 content", Author = "Sami Suliman" },
+    };
+}
---
+++ MinimalApiResponses/Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalApiResponses.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+}
