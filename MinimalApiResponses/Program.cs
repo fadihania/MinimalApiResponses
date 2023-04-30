990b202f5b32df4591750b9314829fddff5ff708
Fadi Hania <fhania@gmail.com>
2023-04-30 21:18:00 +0300

    Add GET endpiont to return all posts using: Results.Ok() to return 200 status code

--- MinimalApiResponses/Program.cs
+++ MinimalApiResponses/Program.cs
@@ -1,6 +1,14 @@
+using MinimalApiResponses;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Json("Blog"));
+
+// Blog API
+app.MapGet("/api/Posts", () =>
+{
+    return Results.Ok(Data.Posts);
+});
 
 app.Run();
