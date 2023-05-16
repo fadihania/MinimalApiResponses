fb84c0f064dffb7fc34216832480862c510418c2
Fadi Hania <fhania@gmail.com>
2023-05-16 07:03:24 +0300

    Create DbContext class and configure connection string

---
+++ MinimalApiResponses/DbData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApiResponses;
+
+public class DbData : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "server=localhost;user=root;password=123456;database=blog", // Connection string
+            new MySqlServerVersion(new Version(5, 7, 42)));             // MySQL version
+    }
+}
